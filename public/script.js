b29ab74f8496b8b96f6a281ea213e35f8149f3f2
Brian Deutsch <bgdeutsch@gmail.com>
2015-12-24 17:11:32 -0500

    Adds styles and some form validation.

--- public/script.js
+++ public/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
 
-$( '.nav .nav-bar li' ).click( function() {
-      var content = $( this ).attr('rel');
-      $( '.panel.active' ).fadeOut(1000, nextPage);
+$('.nav .nav-bar li').click(function() {
+      var content = $(this).attr('rel');
+      $('.page.active').fadeOut(1000, nextPage);
 
       function nextPage(){
         $(this).removeClass('active');
@@ -12,6 +12,31 @@ $( '.nav .nav-bar li' ).click( function() {
         });
      };
   });
+
+// Form validations should generally be done server-side,
+// in case user's browser has jS turned off.  Re-factoring coming soon.
+$('.submit').click(function(e){
+  var regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  var email = $('[data-rel="email"]').val();
+  var username = $('[data-rel="username"]').val();
+  var message = $('[data-rel="message"]').val();
+
+  if ( !regex.test(email) ){
+    e.preventDefault();
+    $('.error').text('Plese enter a valid email!');
+    $('.error').css('visibility', 'visible');
+  }
+  else if ( username == '' ){
+    e.preventDefault();
+    $('.error').text('Please enter your name!');
+    $('.error').css('visibility', 'visible');
+  }
+  else if ( message == ''){
+    e.preventDefault();
+    $('.error').text('Please enter a message!');
+    $('.error').css('visibility', 'visible');
+  }
+});
 
 
 //on document.ready
--- server.js
+++ server.js
@@ -2,11 +2,14 @@ var express = require('express');
 var path = require('path');
 var fs = require('fs');
 var app = new express();
+var validator = require("validator");
 var bodyParser = require('body-parser');
+
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 var server = app.listen(process.env.PORT || 3000);
 
@@ -25,12 +28,10 @@ app.post('/form', function(req, res){
   var user_info = 'Name:' + ' ' + username + '   ' + 'Email:' + ' ' + email + '   ' + 'Message:' + ' ' +message
 
   fs.appendFile('message.txt', user_info+'\n', function (err) {
-    // if (err) throw err;
-
-    if(err){
-  return res.send('err');
+  if(err){
+    return res.send('err');
   }
-  req.flash('Thanks for your message!')
-  return res.redirect('/');
+    return res.redirect('/');
+
   });
 });
