10c347129b1c6be93a503c45cc03bc4dded796d0
Brian Deutsch <bgdeutsch@gmail.com>
2016-02-02 16:46:43 -0500

    Inital commit site redesign.

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
-  //Google Analytics
 
+  //Google Analytics
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -9,66 +9,46 @@ $(document).ready(function(){
 
   ga('create', 'UA-72601012-1', 'auto');
   ga('send', 'pageview');
+  // End Google Analytics script.
 
-  // End Google script.
 
-  $('li').click(function() {
-    var content = $(this).attr('rel');
-    $('.page.active').fadeOut(1000, nextPage);
+  //Scroll to specified div when user clicks anchor tag.
+  $('a[href^="#"]').on('click',function (e) {
+  	    e.preventDefault();
 
-    function nextPage(){
-      $(this).removeClass('active');
-      $('#' + content).fadeIn(1000, function(){
-        $(this).addClass('active');
-      });
-    };
-  });
+  	    var target = this.hash;
+  	    var $target = $(target);
 
-  $('.fa.fa-bars.fa-2x').click(function(e){
-    $('.dropdown').toggleClass('show');
-  });
+  	    $('html, body').stop().animate({
+  	        'scrollTop': $target.offset().top
+  	    }, 1700, 'swing', function () {
+  	        window.location.hash = target;
+  	    });
+  	});
 
-  $('.row').click(function(e){
-    $('.dropdown').toggleClass('show');
-  });
-  //Stop propagation allows menu to remain down when clicked,
-  //Then close it when user clicks out of menu.
-  // $('#burger').click(function(e){
-  //   $('.dropdown-nav').slideToggle(700);
-  //   e.stopPropagation();
-  // });
-  //
-  // $('html').click(function(){
-  //   $('.dropdown-nav').slideUp(700);
-  // });
-  //
-  // $('.row').click(function(){
-  //   $('.dropdown-nav').toggleClass('show');
-  // });
+// Form validations should generally be done server-side,
+// in case user's browser has jS turned off.  Re-factoring coming soon.
+$('.submit').click(function(e){
+  var regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  var email = $('[data-rel="email"]').val();
+  var username = $('[data-rel="username"]').val();
+  var message = $('[data-rel="message"]').val();
 
-  // Form validations should generally be done server-side,
-  // in case user's browser has jS turned off.  Re-factoring coming soon.
-  $('.submit').click(function(e){
-    var regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-    var email = $('[data-rel="email"]').val();
-    var username = $('[data-rel="username"]').val();
-    var message = $('[data-rel="message"]').val();
-
-    if ( !regex.test(email) ){
-      e.preventDefault();
-      $('.error').text('Plese enter a valid email!');
-      $('.error').css('visibility', 'visible');
-    }
-    else if ( username == '' ){
-      e.preventDefault();
-      $('.error').text('Please enter your name!');
-      $('.error').css('visibility', 'visible');
-    }
-    else if ( message == ''){
-      e.preventDefault();
-      $('.error').text('Please enter a message!');
-      $('.error').css('visibility', 'visible');
-    }
-  });
-  //on document.ready
+  if ( !regex.test(email) ){
+    e.preventDefault();
+    $('.error').text('Plese enter a valid email!');
+    $('.error').css('visibility', 'visible');
+  }
+  else if ( username == '' ){
+    e.preventDefault();
+    $('.error').text('Please enter your name!');
+    $('.error').css('visibility', 'visible');
+  }
+  else if ( message == ''){
+    e.preventDefault();
+    $('.error').text('Please enter a message!');
+    $('.error').css('visibility', 'visible');
+  }
+});
+//on document.ready
 });
